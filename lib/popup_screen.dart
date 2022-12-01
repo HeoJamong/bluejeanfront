e3dc0c44374799e3cc227f3d0f65c8ab1c36d37f
허지명 <jm@heojimyeong-ui-MacBookAir.local>
2022-12-01 23:19:14 +0900

    에러 스크린 수정

--- lib/popup_screen.dart
+++ lib/popup_screen.dart
@@ -16,40 +16,11 @@ String randomRoomId() {
   return base64UrlEncode(val);
 }
 
-void emftyRoomIdError(BuildContext context) {
-  var dialog = Dialog(child: EmftyRoomIdErrorScreen());
-  showDialog(
-      context: context,
-      barrierDismissible: false,
-      builder: (BuildContext context) => dialog);
-}
-
-void fullRoomError(BuildContext context) {
-  var dialog = Dialog(child: FullRoomErrorScreen());
-  showDialog(
-      context: context,
-      barrierDismissible: false,
-      builder: (BuildContext context) => dialog);
-}
-
-void roomIdError(BuildContext context) {
-  var dialog = Dialog(child: RoomIdErrorScreen());
-  showDialog(
-      context: context,
-      barrierDismissible: false,
-      builder: (BuildContext context) => dialog);
-}
-
-void emptyNicknameError(BuildContext context) {
-  var dialog = Dialog(child: EmptyNicknameError());
-  showDialog(
-      context: context,
-      barrierDismissible: false,
-      builder: (BuildContext context) => dialog);
-}
-
-void nicknameError(BuildContext context) {
-  var dialog = Dialog(child: NicknameErrorScreen());
+void errorScreen(BuildContext context, String msg) {
+  var dialog = Dialog(
+      child: ErrorScreen(
+    errorMsg: msg,
+  ));
   showDialog(
       context: context,
       barrierDismissible: false,
@@ -97,7 +68,14 @@ class ShowHelpScreen extends StatefulWidget {
 
 class _ShowHelpScreen extends State<ShowHelpScreen> {
   int cnt = 0;
-  List<String> helpTitle = ["진행방식", '터치게임', '밸런스게임', '폭탄 게임', '벌칙 뽑기'];
+  List<String> helpTitle = [
+    '처음이신가요?',
+    '대기방',
+    '터치 게임',
+    '밸런스 게임',
+    '폭탄 게임',
+    '벌칙 뽑기'
+  ];
   List<String> helpContent = [
     "우측 상단 ? 버튼을 눌러 도움말을 확인할 수 있으며 톱니바퀴 버튼을 눌러 설졍 화면으로 이동할 수 있습니다. 방 참가하기 버튼을 누르고 닉네임과 방 코드를 입력하면 대기방에 참가할 수 있으며 방 생성하기 버튼을 누르고 닉네임을 입력하면 대기방을 생성할 수 있습니다.",
     "최상단에 방코드가 나타나 있으며 상단에 참가자 목록, 하단에 게임목록이 나타나 있습니다. 방장은 참가자 목록에 다른 색상으로 표시되며 방장은 하단 게임 목록에서 진행될 게임을 한 가지 선택하여 시작할 수 있습니다.",
@@ -107,15 +85,19 @@ class _ShowHelpScreen extends State<ShowHelpScreen> {
     "탈락한 사람들은 벌칙을 뽑아 나온 벌칙을 뽑아 나온 벌칙을 수행하게 됩니다. 행운을 빌어요"
   ];
   void nextHelp() {
-    setState(() {
-      cnt++;
-    });
+    if (cnt < helpContent.length) {
+      setState(() {
+        cnt++;
+      });
+    }
   }
 
   void beforeHelp() {
-    setState(() {
-      cnt--;
-    });
+    if (cnt > 0) {
+      setState(() {
+        cnt--;
+      });
+    }
   }
 
   @override
@@ -123,60 +105,64 @@ class _ShowHelpScreen extends State<ShowHelpScreen> {
     return Container(
       height: 600,
       width: 400,
-      child: Container(
-        decoration: BoxDecoration(
-          image: DecorationImage(
-              image: AssetImage('assets/helpPage.png'), fit: BoxFit.fill),
-        ),
-        child: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(left: 240),
-              child: IconButton(
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-                icon: Image.asset('assets/closeButton.png'),
-              ),
+      decoration: BoxDecoration(
+        image: DecorationImage(
+            image: AssetImage('assets/helpPage.png'), fit: BoxFit.fill),
+      ),
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(left: 240),
+            child: IconButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              icon: Image.asset('assets/closeButton.png'),
             ),
-            Padding(
-              padding: const EdgeInsets.only(left: 70, top: 20),
-              child: Column(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(
-                        top: 90.0, right: 100, bottom: 30),
-                    child: Text(
-                      "${helpTitle[cnt]}",
-                      style: TextStyle(fontSize: 28, fontFamily: "Retro"),
-                    ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 70, top: 20),
+            child: Column(
+              children: [
+                Padding(
+                  padding:
+                      const EdgeInsets.only(top: 90.0, right: 100, bottom: 30),
+                  child: Text(
+                    helpTitle[cnt],
+                    style: TextStyle(fontSize: 28, fontFamily: "Retro"),
                   ),
-                  SizedBox(
-                    height: 250,
-                    width: 300,
-                    child: Padding(
-                      padding: const EdgeInsets.only(right: 80.0),
-                      child: Text("${helpContent[cnt]}",
-                          style:
-                              TextStyle(fontSize: 16.5, fontFamily: "Retro")),
-                    ),
+                ),
+                SizedBox(
+                  height: 250,
+                  width: 300,
+                  child: Padding(
+                    padding: const EdgeInsets.only(right: 80.0),
+                    child: Text(helpContent[cnt],
+                        style: TextStyle(fontSize: 16.5, fontFamily: "Retro")),
                   ),
-                  Padding(
-                    padding: const EdgeInsets.only(
-                      right: 80.0,
-                    ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(
+                    right: 80.0,
+                  ),
+                  child: SizedBox(
+                    width: 200,
                     child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
-                        GestureDetector(
-                          onTap: beforeHelp,
-                          child: Image.asset(
-                            'assets/beforeButton.png',
-                            width: 75,
-                            height: 40,
+                        Visibility(
+                          visible: (cnt != 0),
+                          child: GestureDetector(
+                            onTap: beforeHelp,
+                            child: Image.asset(
+                              'assets/beforeButton.png',
+                              width: 75,
+                              height: 40,
+                            ),
                           ),
                         ),
-                        Padding(
-                          padding: const EdgeInsets.only(left: 8.0),
+                        Visibility(
+                          visible: (cnt != 5),
                           child: GestureDetector(
                             onTap: nextHelp,
                             child: Image.asset(
@@ -189,45 +175,8 @@ class _ShowHelpScreen extends State<ShowHelpScreen> {
                       ],
                     ),
                   ),
-                ],
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class EmptyNicknameError extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    return Container(
-      height: 300,
-      decoration: BoxDecoration(
-          image: DecorationImage(
-              image: AssetImage('assets/error.png'), fit: BoxFit.fill)),
-      child: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.only(left: 220.0),
-            child: IconButton(
-              onPressed: () {
-                Navigator.pop(context);
-              },
-              icon: Image.asset('assets/closeButton.png'),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(left: 100.0),
-            child: Container(
-              child: Padding(
-                  padding: const EdgeInsets.only(top: 70),
-                  child: Text(
-                    "별명이 입력되지 않았습니다.",
-                    style: TextStyle(fontSize: 27, fontWeight: FontWeight.bold),
-                  )),
+                ),
+              ],
             ),
           ),
         ],
@@ -236,44 +185,9 @@ class EmptyNicknameError extends StatelessWidget {
   }
 }
 
-class NicknameErrorScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    return Container(
-      height: 300,
-      decoration: BoxDecoration(
-          image: DecorationImage(
-              image: AssetImage('assets/error.png'), fit: BoxFit.fill)),
-      child: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.only(left: 220.0),
-            child: IconButton(
-              onPressed: () {
-                Navigator.pop(context);
-              },
-              icon: Image.asset('assets/closeButton.png'),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(left: 100.0),
-            child: Container(
-              child: Padding(
-                  padding: const EdgeInsets.only(top: 70),
-                  child: Text(
-                    "별명이 중복 되었습니다.",
-                    style: TextStyle(fontSize: 27, fontWeight: FontWeight.bold),
-                  )),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class EmftyRoomIdErrorScreen extends StatelessWidget {
+class ErrorScreen extends StatelessWidget {
+  final String errorMsg;
+  ErrorScreen({super.key, required this.errorMsg});
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -298,44 +212,11 @@ class EmftyRoomIdErrorScreen extends StatelessWidget {
               child: Padding(
                   padding: const EdgeInsets.only(top: 70),
                   child: Text(
-                    "방코드가 입력되지 않았습니다.",
-                    style: TextStyle(fontSize: 27, fontWeight: FontWeight.bold),
-                  )),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class RoomIdErrorScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: 300,
-      decoration: BoxDecoration(
-          image: DecorationImage(
-              image: AssetImage('assets/error.png'), fit: BoxFit.fill)),
-      child: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.only(left: 220.0),
-            child: IconButton(
-              onPressed: () {
-                Navigator.pop(context);
-              },
-              icon: Image.asset('assets/closeButton.png'),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(left: 100.0),
-            child: Container(
-              child: Padding(
-                  padding: const EdgeInsets.only(top: 70),
-                  child: Text(
-                    "방코드가 틀립니다.",
-                    style: TextStyle(fontSize: 27, fontWeight: FontWeight.bold),
+                    errorMsg,
+                    style: TextStyle(
+                        fontSize: 27,
+                        fontWeight: FontWeight.bold,
+                        fontFamily: "Retro"),
                   )),
             ),
           ),
@@ -691,14 +572,5 @@ class JoinRoomScreenState extends State<JoinRoomScreen> {
             )
           ])),
     );
-  }
-}
-
-class FullRoomErrorScreen extends StatelessWidget {
-  const FullRoomErrorScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container();
   }
 }
